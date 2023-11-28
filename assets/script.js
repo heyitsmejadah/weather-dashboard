676d5587d09743d21a47a4f4a011371318dc94d1
Jadah Pierce <145732309+heyitsmejadah@users.noreply.github.com>
2023-11-27 21:21:20 -0700

    Fixed the conditions append child elements.

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,77 @@
-var apiKey = "c5b6629140f3fee5f9365a733c6ccf73";
-var searchHistory = [];
+const apiKey = "c5b6629140f3fee5f9365a733c6ccf73";
+const searchHistory = [];
 
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("search-button").addEventListener("click", searchWeather);
 });
 
 function searchWeather() {
-    var city = document.getElementById("city").value;
-    var country = document.getElementById("country").value;
+    const city = document.getElementById("city").value;
+    const country = document.getElementById("country").value;
     getApi(city,country,apiKey);
 }
 
-function getApi(city, countryCode, apiKeyParam) {
-    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${apiKeyParam}`;
-
+function getApi(city, countryCode, apiKey) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${apiKey}&units=imperial`;
+    
     fetch(apiUrl)
         .then(function (response) {
             // error handling for API Key fetch
             if (!response.ok) {
-                alert("API error!");
-            } else {
+                throw new Error("API error!")
+            } 
                 return response.json();
-            }
         })
         .then(function (data) {
-            displayWeather(data);
             console.log(data);
+            displayWeather(data);
         })
         .catch(function (error) {
             // error handling in case of page error
             console.error("Error fetching data:", error);
         });
 }
+// I NEED TO APPEND THE DISPLAYWEATHER ITEMS TO SOMETHING FOR THEM TO APPEAR
 function displayWeather(data) {
-    document.getElementById("city-name").innerText = data.name;
-    document.getElementById("date").innerText = new Date().toLocaleDateString();
-    // FIX THIS!!!!
-    document.getElementById("icon").innerHTML = "img src="<https://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="Weather Icon">`;
-    // FIX THIS to make it fahrenheit
-    document.getElementById("temperature").innerText = "Temperature: ${data.main.temp} degrees celcious";
+    const forecast = document.getElementById("forecast")
+    forecast.innerHTML = "";
+
+    const cityNameElement= document.createElement("h2");
+    cityNameElement.innerText = data.name;
+    forecast.appendChild(cityNameElement);
+
+    const currentDateElement = document.createElement("p");
+    const currentDate = new Date();
+    currentDateElement.innerText = currentDate.toLocaleDateString();
+    forecast.appendChild(currentDateElement)
+   
+    const iconElement = document.createElement("div");
+    iconElement.innerHTML = `<img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="Weather Icon">`;
+    forecast.appendChild(iconElement);
+
+    const temperatureElement = document.createElement("p");
+    temperatureElement.innerText = `${data.main.temp} °F`;
+    forecast.appendChild(temperatureElement);
+
+    if (!searchHistory.includes(data.name)) {
+        searchHistory.push(data.name);
+        updateSearchHistory();
+    }
+
+
+}
+
+function updateSearchHistory() {
+    const historyList = document.getElementById("search-history");
+    historyList.innerHTML = "";
+
+    searchHistory.forEach(function(search) {
+        const [city, country] = search.split(",");
+        const listItem = document.createElement("li");
+        listItem.textContent = search;
+        listItem.addEventListener("click",function() {
+            getApi(city, country, apiKey);
+        });
+        historyList.appendChild(listItem);
+    });
 }
